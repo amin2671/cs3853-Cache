b3eba93ca70b097af2110d47f0e98b1c34d01175
AustinSPena <44416760+AustinSPena@users.noreply.github.com>
2020-04-26 19:24:08 -0500

    Added cache simulation

--- CS3853 Cache Project/src/output/Print.java
+++ CS3853 Cache Project/src/output/Print.java
@@ -31,15 +31,15 @@ public class Print {
 		System.out.printf("Cost:\t\t\t\t$%.2f\n\n", cache.cost());
 				
 		System.out.printf("***** CACHE SIMULATION RESULTS *****\n\n");
-		System.out.printf("Total Cache Accesses:\t%d\n", cache.totalAccess());
-		System.out.printf("Cache Hits:\t\t%d\n", 0);
-		System.out.printf("Cache Misses:\t\t%d\n", 0);
-		System.out.printf("--- Compulsory Misses:\t   %d\n", 0);
-		System.out.printf("--- Conflict Misses:  \t   %d\n\n\n", 0);
+		System.out.printf("Total Cache Accesses:\t%d\n", cache.hitCount + cache.conflictMissCount + cache.compulsoryMissCount);
+		System.out.printf("Cache Hits:\t\t%d\n", cache.hitCount);
+		System.out.printf("Cache Misses:\t\t%d\n", cache.compulsoryMissCount + cache.conflictMissCount);
+		System.out.printf("--- Compulsory Misses:\t   %d\n", cache.compulsoryMissCount);
+		System.out.printf("--- Conflict Misses:  \t   %d\n\n\n", cache.conflictMissCount);
 		
 		System.out.printf("***** ***** CACHE HIT & MISS RATE: ***** *****\n\n");
-		System.out.printf("Hit Rate:\t\t%.4f%%\n", 0.0);
-		System.out.printf("Miss Rate:\t\t%.4f%%\n", 0.0);
+		System.out.printf("Hit Rate:\t\t%.4f%%\n", 100.0 * cache.hitCount / (cache.hitCount + cache.conflictMissCount + cache.compulsoryMissCount));
+		System.out.printf("Miss Rate:\t\t%.4f%%\n", 100.0 * (cache.compulsoryMissCount + cache.conflictMissCount) / (cache.hitCount + cache.conflictMissCount + cache.compulsoryMissCount));
 		System.out.printf("CPI:\t\t\t%.2f Cycles/Instruction\n", 0.0);
 		System.out.printf("Unused Cache Space:\t%.2f KB / %.2f KB = %.2f%%  Waste: $%.2f\n", 0.0, cache.implementSize(), (0.0 * 100) / cache.implementSize(), 0.0);
 		System.out.printf("Unused Cache Blocks:\t%d / %d", 0, cache.totalBlocks());
